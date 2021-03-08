0930c2800d7fc425e33e54e7351244451a8519a5
donatas <d.kadakauskis@gmail.com>
2021-03-08 14:09:11 +0200

    aa

---
+++ src/test/java/building/registry/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package building.registry.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
