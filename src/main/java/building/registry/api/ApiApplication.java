d93f478e0fee340669a0736b082b81b7434a010b
donatas <d.kadakauskis@gmail.com>
2021-03-15 13:03:48 +0200

    Readme.txt

--- src/main/java/building/registry/api/ApiApplication.java
+++ src/main/java/building/registry/api/ApiApplication.java
@@ -2,6 +2,8 @@ package building.registry.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -9,6 +11,26 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatCustomizer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers((connector -> {
+
+			/**
+			 * Gets PORT env variable and sets it as tomcat server port.
+			 * In Heroku env, the PORT env variable must be used by tomcat
+			 * because if not the process will fail.
+			 * Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
+			 */
+			String envPort = System.getenv("PORT");
+			// If PORT is NULL, we'll use the default port
+			if (envPort != null) {
+				connector.setPort(Integer.parseInt(envPort));
+			}
+		}));
+		return factory;
 	}
 
 }
